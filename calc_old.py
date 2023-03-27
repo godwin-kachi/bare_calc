0f46f4b3930bb2fc33834149c8459009a614697e
Godwin Kachi <godwinkachi@hotmail.com>
2023-03-27 13:11:04 +0100

    Updated this repo to contain an optimized version

--- calc_old.py
+++ calc_old.py
@@ -7,9 +7,41 @@ num1 = int(input('Random number 1: '))
 num2 = int(input('Random number 2: '))
 answer = []
 
+def more_evaluation(*num):
+    pass
+
+
+def add(num1, num2):
+    ans = num1 + num2
+    print(f'{num1} + {num2} = ', ans)
+    restart()
+
 def quit():
     print('Thank you for using our app')
     print('Bye Bye')
+
+def minus(num1, num2):
+    print(f'{num1} - {num2} = ', num1 - num2)
+    restart()
+
+def divide(num1, num2):
+    if num2 == 0:
+        print('Unfortunately I cant divide by zero')
+        restart()
+    else:
+        print(f'{num1} / {num2} = ', num1/num2 )
+        restart()
+
+def multiply(num1, num2):
+    print(f'{num1} x {num2} = ', num1*num2)
+    restart()
+
+def raises(num1, num2):
+    print(f'{num1} ** {num2} = ', num1**num2)
+    restart()
+
+
+
 
 def restart():
     print('''What would you like to do:
@@ -21,28 +53,26 @@ def restart():
     6. Quit
         ''')
 
+    
+
     try:
         choice = int(input('choice: '))
         match choice:
             case 1:
-                print(f'{num1} + {num2} = ', num1 + num2)
+                add(num1, num2)
             case 2:
-                print(f'{num1} - {num2} = ', num1 - num2)
+                minus(num1, num2)
             case 3:
-                if num2 == 0:
-                    print('Unfortunately I cant divide by zero')
-                    restart()
-                else:
-                    print(f'{num1} / {num2} = ', num1/num2 )
-                    restart()
+                divide(num1, num2)
             case 4:
-                print(f'{num1} x {num2} = ', num1*num2)
+                multiply(num1, num2)
             case 5:
-                print(f'{num1} ** {num2} = ', num1**num2)
+                raises(num1, num2)
             case 6:
                 quit()
             case _:
                 print('You have selected an option I do not recognize')
+                quit()
     except ValueError:
         print('I am programmed to accept digits and not text')
         quit()
